fe1045c626f579eb617b2aaebe26efd64e22c1d0
ambika-chukanatti <67265606+ambika-chukanatti@users.noreply.github.com>
2026-05-16 11:30:39 +0530

    design changes to page.tsx

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,9 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
+      dbName: 'imaginify', 
+      bufferCommands: false,
+      family: 4 
     })
 
   cached.conn = await cached.promise;
